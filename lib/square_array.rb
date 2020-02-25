4cec25b1ea0afc181d73a7885dcf65ef9c74be97
Duncan Uruchima <duruchima1@gmail.com>
2020-02-25 16:20:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
     counter=0
     n=array
     while array[counter] do
-    new_array=Math.sqrt(n)
+    new_array=Math.sqrt(array)
     counter+=1
     return new_array
   end
